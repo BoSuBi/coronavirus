de1cffd18b2a5edceb3215a42b1a1d59fa0b412a
Hiệp Nguyễn <hiepnh.dev@gmail.com>
2021-07-30 16:20:35 +0700

    commit

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:coronavirus/app/repositories/endpoints_data.dart';
 import 'package:coronavirus/app/services/api.dart';
 import 'package:coronavirus/app/services/api_service.dart';
 import 'package:http/http.dart';
@@ -18,5 +19,36 @@ class DataRepository {
       }
       rethrow;
     }
+  }
+
+  Future<EndpointsData> getAllEndpointData() async {
+    try {
+      return await _getAllEndpointData();
+    } on Response catch (response) {
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await _getAllEndpointData();
+      }
+      rethrow;
+    }
+  }
+
+  Future<EndpointsData> _getAllEndpointData() async {
+    final values = await Future.wait([
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+    return EndpointsData(
+      values: {
+        Endpoint.cases: values[0],
+        Endpoint.casesSuspected: values[1],
+        Endpoint.casesConfirmed: values[2],
+        Endpoint.deaths: values[3],
+        Endpoint.recovered: values[4],
+      },
+    );
   }
 }
